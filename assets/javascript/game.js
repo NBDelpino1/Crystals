9a1b6e330498a0fa302d9458dfac3c9f0af06058
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-07-22 22:33:03 -0400

    smh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -55,19 +55,6 @@ $(document).ready(function(){
 
 
 
-
-//             var numscore = 0;
-//             function myFunction(p1, p2) {
-//     return p1 * p2;              // The function returns the product of p1 and p2
-// }
-
-
-
-
-
-
-
-
         });    
 
       
